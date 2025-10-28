a7ce638e6e71d42d60736767c77e18efc8e88b8a
Jasmine Caicedo <jecaiced@uci.edu>
2025-10-27 19:12:09 -0700

    changes me and my groupmate made to DeerW5

--- In Class Activities/Assets/W5/Scripts/DeerW5.cs
+++ In Class Activities/Assets/W5/Scripts/DeerW5.cs
@@ -1,7 +1,28 @@
+using UnityEditor.AI;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering;
 
 // Write your DeerW5 class in here :)
 // Hint: if you don't remember what a class is supposed to look like,
 //      maybe check out CatW5...
 // If you copied the class declaration from CatW5, you'd only need to change one thing...
+
+public class DeerW5 : MonoBehaviour
+{
+    //member variables that will be used: target, movement speed, position
+
+    [SerializeField] private float _movespeed = 1.0f;
+    [SerializeField] private Transform _deerPosition;
+    [SerializeField] private NavMeshAgent _target;
+    private bool _hitTarget;
+
+    //make an updat function 
+    void Start()
+    {
+        Vector3 Target = new Vector3(31.533f, 1.05f, 20.966f);
+        _target.SetDestination(Target);
+    }
+    
+
+}
