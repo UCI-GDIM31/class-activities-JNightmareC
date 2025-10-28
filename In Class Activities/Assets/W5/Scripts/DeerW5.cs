e9e040eedfef29edbe853e2bc33cafadd1a8ecfa
Jasmine Caicedo <jecaiced@uci.edu>
2025-10-27 20:12:23 -0700

    Finished deer script

--- In Class Activities/Assets/W5/Scripts/DeerW5.cs
+++ In Class Activities/Assets/W5/Scripts/DeerW5.cs
@@ -8,22 +8,33 @@ using UnityEngine.Rendering;
 //      maybe check out CatW5...
 // If you copied the class declaration from CatW5, you'd only need to change one thing...
 
+// The ": MonoBehaviour" after the class name is what makes it useable as a Component.
 public class DeerW5 : MonoBehaviour
 {
-    //member variables that will be used: target, movement speed, position
+    // This is a member variable.
+    // The "[SerializeField] private" is what makes this variable tunable from
+    //      the Inspector.
+    // "Transform" defines the type of this variable - it's a Transform, the
+    //      Component that stores each GameObject's position, rotation, and scale.
+    [SerializeField] private Transform _destination;
+    [SerializeField] private NavMeshAgent agent;
 
-    [SerializeField] private float _movespeed = 1.0f;
-    [SerializeField] private Transform _deerPosition;
-    [SerializeField] private NavMeshAgent _target;
-    private bool _hitTarget;
-
-    //make an updat function 
-    void Start()
+    // ------------------------------------------------------------------------
+    // The Start method is called when the game begins.
+    private void Start()
     {
-        NavMeshAgent agent = 
-        Vector3 Target = new Vector3(31.533f, 1.05f, 20.966f);
-        _target.SetDestination(Target);
+        // This is how we use GetComponent to get the NavMeshAgent Component
+        //      on this GameObject, and store it in a variable named "agent".
+        agent = this.GetComponent<NavMeshAgent>();
+        // This is how we use the NavMeshAgent to tell the GameObject where to
+        //      move towards.
+        // Notice how we get the "position" variable of _destination;
+        //      that's because _destination is a Transform, which contains 3
+        //      variables: position, rotation, and scale.
     }
-    
+    void Update()
+    {
+        agent.SetDestination(_destination.position);
 
+    }
 }
